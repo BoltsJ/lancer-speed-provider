c3456d4a930314695dd39494ba176808c8917659
Joe Nosie <182554+BoltsJ@users.noreply.github.com>
2021-09-05 19:25:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Lancer Speed Provider
+
+A [Drag Ruler](https://foundryvtt.com/packages/drag-ruler) speed provider for lancer. It features integration with conditions to determine if a token can move and if so how many moves it can take. In the case of player mechs and npcs with the Limitless trait, it also shows a third range for units that can overcharge, indicating spending overcharge to boost.
+
+## Installation
+
+Paste the following manifest url into the foundry module installer:
+
+```
+manifest: https://github.com/BoltsJ/lancer-speed-provider/releases/latest/download/module.json
+```
---
+++ lancer-provider.js
@@ -0,0 +1,83 @@
+Hooks.once("dragRuler.ready", (SpeedProvider) => {
+  class LancerSpeedProvider extends SpeedProvider {
+    get colors() {
+      return [
+        {
+          id: "standard",
+          default: 0x1a8789,
+          name: "lancer-speed-provider.standard",
+        },
+        {
+          id: "boost",
+          default: 0xc2a13a,
+          name: "lancer-speed-provider.boost",
+        },
+        {
+          id: "over-boost",
+          default: 0xbe642a,
+          name: "lancer-speed-provider.over-boost",
+        },
+      ];
+    }
+
+    /**
+     * @param token {Token}
+     */
+    getRanges(token) {
+      // Cant move if stunned or immobilized
+      const actor = token.actor;
+      const effects = getStatusIds(actor);
+      const stunned =
+        effects.filter(
+          (e) =>
+            e?.endsWith("stunned") ||
+            e?.endsWith("immobilized") ||
+            e?.endsWith("shutdown") ||
+            e?.endsWith("downandout")
+        ).length > 0;
+      if (stunned) return [{ range: 0, color: "standard" }];
+
+      // Can't boost if stunned
+      /**@type{number}*/
+      const speed = actor.data.data.derived.speed;
+      const slowed = effects.filter((e) => e?.endsWith("slow")).length > 0;
+
+      const ranges = [{ range: speed, color: "standard" }];
+      if (!slowed && !actor.is_pilot())
+        ranges.push({ range: speed * 2, color: "boost" });
+      if (!slowed && canOvercharge(actor))
+        ranges.push({ range: speed * 3, color: "over-boost" });
+
+      return ranges;
+    }
+
+    /**
+     * TODO: Figure out what ignores difficult terrain
+    getCostForStep(token, area, options={}) {
+      return super.getCostForStep(token, area, options);
+    }
+    */
+  }
+
+  dragRuler.registerModule("lancer-speed-provider", LancerSpeedProvider);
+});
+
+/**
+ * @returns {boolean}
+ */
+function canOvercharge(actor) {
+  if (actor.is_npc()) {
+    const limitless = actor.itemTypes.npc_feature.find(
+      (i) => i.name === "LIMITLESS"
+    );
+    return !!limitless;
+  }
+  return actor.is_mech();
+}
+
+/**
+ * @returns {Array<string>}
+ */
+function getStatusIds(actor) {
+  return actor.effects.map((e) => e.getFlag("core", "statusId"));
+}
